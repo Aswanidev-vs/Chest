f73e982d978efaca23c6f1ed34751e304aa4cc45
Aswanidev <apploginer123@gmail.com>
2026-09-05 13:03:24 +0530

    chore: remove hardcoded commitsha and use debug.ReadBuildInfo

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -3,21 +3,40 @@ package cli
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
-const (
-	Version   = "v0.1.0"
-	CommitSHA = "dev"
-)
+var Version = "v0.1.0"
+
+func getCommitSHA() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	for _, setting := range info.Settings {
+		if setting.Key == "vcs.revision" {
+			if len(setting.Value) > 7 {
+				return setting.Value[:7]
+			}
+			return setting.Value
+		}
+	}
+	return ""
+}
 
 func newVersionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print CHEST version and build information",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("CHEST %s (%s/%s)\n", Version, runtime.GOOS, runtime.GOARCH)
+			sha := getCommitSHA()
+			if sha != "" {
+				fmt.Printf("CHEST %s (%s) (%s/%s)\n", Version, sha, runtime.GOOS, runtime.GOARCH)
+			} else {
+				fmt.Printf("CHEST %s (%s/%s)\n", Version, runtime.GOOS, runtime.GOARCH)
+			}
 		},
 	}
 }
